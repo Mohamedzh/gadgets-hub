e27c67befe9fde1d9c8d789f831501387eb0c12a
Mohamed Zahran <mohamed.zahran1@live.com>
2022-11-28 17:45:03 +0200

    modified phone links

--- components/phoneList.tsx
+++ components/phoneList.tsx
@@ -1,15 +1,19 @@
 import { PhoneSummary } from '../pages/brands/[brand]'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 
 function PhoneList({ currentPhones }: { currentPhones: PhoneSummary[] }) {
+    const router = useRouter()
+    const [arLang, setArLang] = useState<boolean>(false)
+    useEffect(() => { if (router.asPath.includes('/ar')) { setArLang(true) } }, [router.asPath])
 
     return (
         <div>
             <div className='grid grid-cols-2 lg:grid-cols-4'>
                 {currentPhones.map((phone, i) =>
-                    <Link href={`/${phone.name}/ar`} key={i}>
+                    <Link href={`${arLang ? '/' + phone.name + '/ar' : '/' + phone.name}`} key={i}>
                         <a
                             className='bg-white m-4 p-3 border border-solid flex flex-col place-items-center transition duration-500 ease-in-out hover:scale-110 hover:opacity-75'
                         >
