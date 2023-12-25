05213d969f9a0bfd63797f5b4ae72c282edc55ee
Mohamed Zahran <mohamed.zahran1@live.com>
2023-12-25 21:34:40 +0200

    removed vercel speed insights package

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -2,14 +2,12 @@ import { useRouter } from "next/router";
 import React, { ReactNode } from "react";
 import Footer from "./footer";
 import Navbar from "./navbar";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function Layout({ children }: { children: ReactNode }) {
   const router = useRouter();
 
   return (
     <div className="min-h-full">
-      <SpeedInsights/>
       <Navbar />
       <main>{children}</main>
       <Footer />
