dbe49075cd8c98487af7e8c932acdd96a5695b14
Mohamed Zahran <mohamed.zahran1@live.com>
2023-12-25 21:27:48 +0200

    added vercel speed insights

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -2,12 +2,14 @@ import { useRouter } from "next/router";
 import React, { ReactNode } from "react";
 import Footer from "./footer";
 import Navbar from "./navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function Layout({ children }: { children: ReactNode }) {
   const router = useRouter();
 
   return (
     <div className="min-h-full">
+      <SpeedInsights/>
       <Navbar />
       <main>{children}</main>
       <Footer />
